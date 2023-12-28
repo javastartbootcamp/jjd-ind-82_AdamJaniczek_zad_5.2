0534c6a7a370c28d0925b6d4148ee3c08c6ef638
AdamJaniczek <152891309+AdamJaniczek@users.noreply.github.com>
2023-12-28 19:01:23 +0100

    Rozwiązanie zadania

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -3,6 +3,11 @@ package pl.javastart.task;
 public class Main {
 
     public static void main(String[] args) {
-
+        Room room = new Room(35, 23.8, 20.0, true);
+        room.lowertemperature();
+        room.lowertemperature();
+        room.lowertemperature();
+        room.lowertemperature();
+        System.out.println(room.lowertemperature());
     }
 }
